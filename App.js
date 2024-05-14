6a19556745e11909a18b667b4086690b3abd9934
Himanshu Bhalla <himanshubhalla@Himanshus-MacBook-Pro-2.local>
2024-05-14 22:39:12 +0530

    Added Parcel as project bundler and configured initial settings

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+  return (
+    <>
+      <h1>Himanshu</h1>
+    </> 
+  );
+};
+
+export default AppLayout;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
